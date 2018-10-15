c12a8002ed3c53ee3b6ff66348bd6b3ff3b655cb
niuyu <niuyush@sinosoft.com.cn>
2018-10-15 13:43:05 +0800

    初始化

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-apk --no-cache add tzdata && \ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && \ echo "Asia/Shanghai" > /etc/timezone 
-
 redis-server /app/data/etc/redis.conf --protected-mode no
 
 sed -i "s/%master-ip%/${REDIS_MASTER_IP}/" /app/data/etc/sentinel.conf
