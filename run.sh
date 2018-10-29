ddf7b8844d30e1ea2ab2a8536ec4fd5c1e568796
niuyu <niuyush@sinosoft.com.cn>
2018-10-29 14:37:00 +0800

    初始化

--- run.sh
+++ run.sh
@@ -5,10 +5,3 @@ ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && echo "Asia/Shanghai" 
 sed -i "s/%redispass%/${REDIS_PASSWORD}/" /app/data/etc/redis.conf
 
 redis-server /app/data/etc/redis.conf --protected-mode no
-
-sed -i "s/%master-ip%/${REDIS_MASTER_IP}/" /app/data/etc/sentinel.conf
-sed -i "s/%master-port%/${REDIS_MASTER_PORT}/" /app/data/etc/sentinel.conf
-sed -i "s/%slave-num%/${REDIS_SLAVE_NUM}/" /app/data/etc/sentinel.conf
-sed -i "s/%redispass%/${REDIS_PASSWORD}/" /app/data/etc/sentinel.conf
-
-redis-sentinel /app/data/etc/sentinel.conf
